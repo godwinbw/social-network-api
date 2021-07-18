78cce43fa6f9f85ad81645f532eb428bdbff9e6d
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-07-18 10:18:25 -0500

    added user CRUD

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,27 +7,83 @@ const notImplemented = {
 const userController = {
   // get all users
   getAllUser(req, res) {
-    res.json(notImplemented);
+    User.find({})
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbData) => res.json(dbData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   },
 
   // get a single user by _id and populated thought and friend data
   getUserById(req, res) {
-    res.json(notImplemented);
+    User.findOne({ _id: req.params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbData) => {
+        if (!dbData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   },
 
   // post a new user
   createUser(req, res) {
-    res.json(notImplemented);
+    User.create(req.body)
+      .then((dbData) => res.json(dbData))
+      .catch((err) => res.status(400).json(err));
   },
 
   // put to update a user by _id
   updateUser(req, res) {
-    res.json(notImplemented);
+    User.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbData) => {
+        if (!dbData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch((err) => res.status(400).json(err));
   },
 
   // delete to remove a user by _id
   deleteUser(req, res) {
-    res.json(notImplemented);
+    User.findOneAndDelete({ _id: req.params.id })
+      .then((dbData) => {
+        if (!dbData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch((err) => res.status(400).json(err));
   },
 
   // post to add a new friend to a user's friend list
